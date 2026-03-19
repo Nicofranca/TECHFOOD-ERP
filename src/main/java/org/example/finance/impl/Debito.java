93211c5104ee28d738cd529a9869c473e5c6eb85
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-19 16:40:11 -0300

    feat: implementa strategy de pagamentos

--- src/main/java/org/example/finance/impl/Debito.java
+++ src/main/java/org/example/finance/impl/Debito.java
@@ -1,15 +1,15 @@
 package org.example.finance.impl;
 
-import org.example.finance.MetodoPagamento;
+import org.example.finance.strategy.MetodoPagamento;
 
 public class Debito implements MetodoPagamento {
     @Override
     public boolean suporta(int opcao) {
-        return false;
+        return opcao == 2;
     }
 
     @Override
-    public double calculaDesconto() {
-        return 0;
+    public double calculaDesconto(Double valorBase) {
+        return 0.8;
     }
 }
