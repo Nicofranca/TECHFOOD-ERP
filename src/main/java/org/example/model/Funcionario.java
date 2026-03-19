963a3503c98c9f6d532eb3092db24f288ceafb73
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-19 14:41:49 -0300

    feat: cria entidades principais

---
+++ src/main/java/org/example/model/Funcionario.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+public class Funcionario {
+    private String nome;
+    private String cargo;
+    private Double salario;
+
+
+    public Funcionario(String nome, String cargo, Double salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public Funcionario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+}
---
+++ src/main/java/org/example/model/Prato.java
@@ -0,0 +1,40 @@
+package org.example.model;
+
+public class Prato {
+    private String nome;
+    private Double preco;
+    private int esetoque;
+
+    public Prato(String nome, Double preco, int esetoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.esetoque = esetoque;
+    }
+
+    public Prato() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public int getEsetoque() {
+        return esetoque;
+    }
+
+    public void setEsetoque(int esetoque) {
+        this.esetoque = esetoque;
+    }
+}
